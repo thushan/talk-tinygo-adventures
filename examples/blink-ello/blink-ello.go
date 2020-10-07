33c50271d4274817598496777fb610dbb0c2c6c4
Thushan Fernando <thushan@thushanfernando.com>
2020-10-07 21:00:58 +1100

    examples for demo.

---
+++ examples/blink-ello/blink-ello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+func main() {
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		led.Low()
+		time.Sleep(time.Millisecond * 1000)
+
+		led.High()
+		time.Sleep(time.Millisecond * 1000)
+		for i := 0; i < 5; i++ {
+			led.Low()
+			time.Sleep(time.Millisecond * 250)
+
+			led.High()
+			time.Sleep(time.Millisecond * 250)
+		}
+	}
+}
---
+++ examples/go.mod
@@ -0,0 +1,3 @@
+module github.com/thushan/talk-iot-adventures-with-tinygo
+
+go 1.15
